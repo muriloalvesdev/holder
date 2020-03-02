9d5702ee152e46d0372e50fd78b57a766fbc9e77
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-02 13:54:52 -0300

    removendo rabitmq

--- holder/src/main/java/br/com/cardholder/controller/HolderController.java
+++ holder/src/main/java/br/com/cardholder/controller/HolderController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.cardholder.dto.CardHolderDataTransferObject;
 import br.com.cardholder.dto.RequestResource;
 import br.com.cardholder.service.HolderService;
@@ -15,21 +14,19 @@ import br.com.cardholder.service.HolderService;
 @RequestMapping("api/holder")
 public class HolderController {
 
-    @Autowired
-    private HolderService validateTransactionService;
+  @Autowired
+  private HolderService validateTransactionService;
 
-    @PostMapping("accomplish/sale")
-    public ResponseEntity<Object> findBankAndCardHolderAndAccomplishSale(
-            @RequestBody RequestResource request) {
-        return validateTransactionService
-                .findBankAndCardHolderAndAccomplishSale(request);
-    }
+  @PostMapping("accomplish/sale")
+  public ResponseEntity<Object> findBankAndCardHolderAndAccomplishSale(
+      @RequestBody RequestResource request) {
+    return validateTransactionService.findBankAndCardHolderAndAccomplishSale(request);
+  }
 
-    @PostMapping("save")
-    public ResponseEntity<Object> save(
-            @RequestBody CardHolderDataTransferObject cardHolderDTO) {
-        validateTransactionService.save(cardHolderDTO);
-        return ResponseEntity.ok().build();
-    }
+  @PostMapping("save")
+  public ResponseEntity<Object> save(@RequestBody CardHolderDataTransferObject cardHolderDTO) {
+    validateTransactionService.save(cardHolderDTO);
+    return ResponseEntity.ok().build();
+  }
 
 }
