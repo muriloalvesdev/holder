bebf892299b46e890c6b5ba4b6e6b0157f60be9d
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-02 23:33:57 -0300

    scheduling

--- src/main/java/br/com/cardholder/controller/HolderController.java
+++ src/main/java/br/com/cardholder/controller/HolderController.java
@@ -2,6 +2,7 @@ package br.com.cardholder.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,11 @@ public class HolderController {
   public ResponseEntity<Object> save(@RequestBody CardHolderDataTransferObject cardHolderDTO) {
     validateTransactionService.save(cardHolderDTO);
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/scheduling")
+  public ResponseEntity<String> scheduling() {
+    return ResponseEntity.ok("OK");
   }
 
 }
