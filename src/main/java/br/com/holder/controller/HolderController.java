1ed3d049f72d5f7a6283113f38783352a8a74259
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 17:00:41 -0300

    api deve validar se os campos do requestBody estão todos preenchidos

--- src/main/java/br/com/holder/controller/HolderController.java
+++ src/main/java/br/com/holder/controller/HolderController.java
@@ -2,6 +2,7 @@ package br.com.holder.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +20,13 @@ public class HolderController {
   private HolderService validateTransactionService;
 
   @PostMapping("accomplish/sale")
-  public ResponseEntity<Object> accomplishSale(@RequestBody RequestResource request) {
+  public ResponseEntity<Object> accomplishSale(@Validated @RequestBody RequestResource request) {
     return validateTransactionService.accomplishSale(request);
   }
 
   @PostMapping("save")
-  public ResponseEntity<Object> save(@RequestBody CardHolderDataTransferObject cardHolderDTO) {
+  public ResponseEntity<Object> save(
+      @Validated @RequestBody CardHolderDataTransferObject cardHolderDTO) {
     validateTransactionService.save(cardHolderDTO);
     return ResponseEntity.ok().build();
   }
